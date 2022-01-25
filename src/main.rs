e2809962ea032a13cafe1ffc29849672361c9530
Yesterday17 <mmf@mmf.moe>
2022-01-26 00:57:20 +0800

    Auto reload with --reload

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,26 @@
 mod config;
+mod watch;
 
 use std::path::PathBuf;
 use std::str::FromStr;
 use actix_web::{get, web, App, HttpServer, Responder, HttpResponse};
 use clap::Parser;
 use uuid::Uuid;
-use crate::config::{AppState, Config};
 use askama_actix::TemplateIntoResponse;
+use parking_lot::RwLock;
+use crate::config::{AppState, Config};
+use crate::watch::watch;
 
 #[get("/")]
-async fn index(data: web::Data<AppState>) -> impl Responder {
-    HttpResponse::Found().header("location", format!("/{}", data.first())).finish()
+async fn index(data: web::Data<RwLock<AppState>>) -> impl Responder {
+    HttpResponse::Found().header("location", format!("/{}", data.read().first())).finish()
 }
 
 #[get("/{uuid}")]
-async fn quiz(web::Path(uuid): web::Path<String>, data: web::Data<AppState>) -> impl Responder {
+async fn quiz(web::Path(uuid): web::Path<String>, data: web::Data<RwLock<AppState>>) -> impl Responder {
     match Uuid::from_str(&uuid) {
         Ok(uuid) => {
-            data.get(&uuid).unwrap().into_response()
+            data.read().get(&uuid).unwrap().into_response()
         }
         Err(_) => Ok(HttpResponse::NotFound().finish())
     }
@@ -33,6 +36,9 @@ struct Args {
     /// Path to the config uuid lock file
     #[clap(short, long)]
     lock: Option<PathBuf>,
+
+    #[clap(short, long)]
+    watch: bool,
 }
 
 #[actix_web::main]
@@ -42,8 +48,11 @@ async fn main() -> std::io::Result<()> {
         Some(lock_path) => lock_path,
         None => args.config.with_extension("lock"),
     };
+    let data = web::Data::new(RwLock::new(Config::new(&args.config, &lock_path).into_state()));
 
-    let data = web::Data::new(Config::new(&args.config, &lock_path).into_state());
+    if args.watch {
+        watch(&args.config, &lock_path, data.clone());
+    }
     HttpServer::new(move || App::new()
         .app_data(data.clone())
         .service(index)
---
+++ src/watch.rs
@@ -0,0 +1,36 @@
+use std::path::Path;
+use std::process::exit;
+use notify::{Watcher, RecursiveMode, watcher, DebouncedEvent};
+use std::sync::mpsc::channel;
+use std::time::Duration;
+use parking_lot::RwLock;
+use crate::{AppState, Config};
+
+pub fn watch<P: AsRef<Path>>(path: P, lock: P, data: actix_web::web::Data<RwLock<AppState>>) {
+    let (tx, rx) = channel();
+
+    let path = path.as_ref().canonicalize().unwrap();
+    let lock_path = lock.as_ref().canonicalize().unwrap();
+
+    std::thread::spawn(move || {
+        let mut watcher = watcher(tx, Duration::from_secs(1)).unwrap();
+        watcher.watch(&path, RecursiveMode::NonRecursive).unwrap();
+        loop {
+            match rx.recv() {
+                Ok(event) => match event {
+                    DebouncedEvent::Write(_) => {
+                        // reload data
+                        let mut write = data.write();
+                        *write = Config::new(&path, &lock_path).into_state();
+                        eprintln!("Config reloaded");
+                    }
+                    _ => {}
+                },
+                Err(e) => {
+                    eprintln!("watch error: {:?}", e);
+                    exit(1);
+                }
+            }
+        }
+    });
+}
